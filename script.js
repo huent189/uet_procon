308a03ae43c4b0ad3dadd7b3132a6c052c507a32
huent189 <hueahgase189@gmail.com>
2019-10-17 10:11:08 +0700

    added move and tỉme

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var username;
-var password;
+var username = "team4";
+var password = "PptzOnKP";
 var token = "";
 var matchID = 0;
 var myID;
@@ -7,11 +7,16 @@ var mine;
 var competitor;
 var agentSelected = false;
 var tiled;
+var submitData = {};
+var startTime;
+var matchTime = {"start" : 0, "duration" : 0, "turn" : 0, "totalDuration" : 0};
 function drawingMatch(data) {
     let height = data.height;
     let width = data.width;
     let point = data.points;
     tiled = data.tiled;
+    console.log(tiled);
+    
     if(data.teams[0].teamID === myID){
         mine = data.teams[0];
         competitor = data.teams[1];
@@ -19,6 +24,11 @@ function drawingMatch(data) {
         mine = data.teams[1];
         competitor = data.teams[0];
     }
+    $("#myPoint").html(mine.areaPoint + mine.tilePoint);
+    $("#competitorPoint").html(competitor.areaPoint + competitor.tilePoint);
+    
+    matchTime.start = data.startedAtUnixTime;
+    console.log(data.startedAtUnixTime);
     html = "";
     for (let i = 1; i <= height; i++) {
         let tr = "";
@@ -44,33 +54,16 @@ function drawingMatch(data) {
     for (let i = 0; i < mine.agents.length; i++) {
         let x = mine.agents[i].x;
         let y = mine.agents[i].y;
+        submitData[mine.agents[i].agentID] = {"agentID" : mine.agents[i].agentID, "dx" : 0, "dy" : 0, "type" : "stay"};
         document.getElementById(x + "_" + y).style.background = "lightskyblue";
         document.getElementById(x + "_" + y).setAttribute('onclick', 'selectAgent(' + mine.agents[i].agentID + ',' + x + ',' + y + ')');
+        document.getElementById(x + "_" + y).setAttribute('agent', mine.agents[i].agentID);
     }
     for (let i = 0; i < competitor.agents.length; i++) {
         let x = competitor.agents[i].x;
         let y = competitor.agents[i].y;
         document.getElementById(x + "_" + y).style.background = "greenyellow";
     }
-}
-function K_click(x, y) {
-    for (let i = 0; i < mine.agents.length; i++) {
-        let x1 = mine.agents[i].x;
-        let y1 = mine.agents[i].y;
-        let tmp = Math.abs(x1 - x) + Math.abs(y1 - y);
-        if (tmp <= 1) {
-            document.getElementById(x + "_" + y).style.background = "#e52efa";
-            document.getElementById(x + "_" + y).setAttribute('onclick', 'D_click(' + x + ',' + y + ')');
-        }
-        else {
-            if (Math.abs(x1 - x) === 1 && Math.abs(y1 - y) === 1) {
-                document.getElementById(x + "_" + y).style.background = "#e52efa";
-                document.getElementById(x + "_" + y).setAttribute('onclick', 'D_click(' + x + ',' + y + ')');
-            }
-        }
-
-    }
-
 }
 function selectAgent(id, x, y) {
     console.log("Agent selected");
@@ -80,7 +73,7 @@ function selectAgent(id, x, y) {
         for(let dy = -1; dy < 2; dy++){
             var curX = x + dx;
             var curY = y + dy;
-            if(curX < 1 || curY < 1 || curX > tiled.width || curY > tiled.height) {
+            if(curX < 1 || curY < 1 || curX > tiled.length || curY > tiled.length   ) {
                 continue;
             }
             if(tiled[curX - 1][curY - 1] === competitor.teamID){
@@ -96,44 +89,42 @@ function selectAgent(id, x, y) {
 }
 function action(agentID, dx, dy, type) {
     if(agentSelected){
-        $.ajax({
-            url: "http://sv-procon.uet.vnu.edu.vn:3000/matches/" + matchID + "/action",
-            type: 'post',
-            // dataType: 'json',
-            data: {
-                token : token,
-                actions : {
-                    agentID : agentID,
-                    dx : dx,
-                    dy : dy,
-                    type : type 
-                }
-            },
-            success: function (data) {
-                console.log(data);
-            },
-            statusCode: {
-                401: function () {
-                    alert("token sai hoặc không tồn tại");
-                },
-                400: function (xhr) {
-                    alert("InvalidMatches  or TooEarly: ");
-                    log(xhr.responseText);
-                },
-    
-            }
-            
-        });
+        $("[agent = '" + agentID +"']" ).css("background", "yellow");
+        $("[onclick *= 'action(" + agentID + "," + dx + "," + dy + "']").css("background", "lightskyblue");
+        submitData[agentID] = {"agentID" : agentID, "dx" : dx, "dy" : dy, "type" : type};
     }
     agentSelected = false;
 }
-function D_click(x, y) {
-    document.getElementById(x + "_" + y).style.background = "white";
-    document.getElementById(x + "_" + y).setAttribute('onclick', 'K_click(' + x + ',' + y + ')');
+function run() {
+    $.ajax({
+        url: "http://sv-procon.uet.vnu.edu.vn:3000/matches/" + matchID + "/action",
+        type: 'post',
+        // dataType: 'json',
+        data: {
+            token : token,
+            actions : submitData
+        },
+        success: function (data) {
+            console.log(data);
+        },
+        statusCode: {
+            401: function () {
+                alert("token sai hoặc không tồn tại");
+            },
+            400: function (xhr) {
+                alert("InvalidMatches  or TooEarly: ");
+                log(xhr.responseText);
+            },
+
+        }
+        
+    });
 }
 function processMatchInfoBefore(data) {
     info = "";
     myID = data[0].teamID;
+    matchTime.duration = data[0].turnMillis;
+    matchTime.totalDuration = matchTime.duration + data[0].turnMillis;
     for (let i = 0; i < data.length; i++) {
         info += "ID trận đấu: " + data[i].id;
         info += "<br> Đối thủ: " + data[i].matchTo;
@@ -202,6 +193,9 @@ function getCurrentMatchInfo() {
         }
     });
 }
+function updateTime(params) {
+    
+}
 function getMatch(id) {
     matchID = id;
     getCurrentMatchInfo();
@@ -211,5 +205,17 @@ $(document).ready(function () {
     login().then(function () {
         console.log(token);
         getMatchBefore(token);
-    })
+    });
+    setInterval(function () {
+        if(matchTime.start != 0)
+        {
+            curTurn = Math.floor((Date.now() - matchTime.start)/ matchTime.totalDuration);
+            if(matchTime.turn != curTurn){
+                // getCurrentMatchInfo();
+                matchTime.turn = curTurn;
+                $("#turnNumber").html(matchTime.turn);
+            }
+            $("#remainingTime").html((matchTime.duration - (Date.now() - matchTime.start - matchTime.turn * matchTime.totalDuration))/1000);
+        }
+    }, 400);
 });
